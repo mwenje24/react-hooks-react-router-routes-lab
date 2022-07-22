3a643b88e36ea3802294068f7fda348d433091d4
Mwenje <mwenje24@gmail.com>
2022-07-22 17:27:28 +0300

    added a class to navlinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div>
-      <Link to="/"> Home </Link>
-      <Link to="/movies"> Movies </Link>
-      <Link to="/directors"> Directors </Link>
-      <Link to="/actors"> Actors </Link>
+    <div className="navbar">
+      <NavLink to="/"> Home </NavLink>
+      <NavLink to="/movies"> Movies </NavLink>
+      <NavLink to="/directors"> Directors </NavLink>
+      <NavLink to="/actors"> Actors </NavLink>
     </div>
   );
 }
